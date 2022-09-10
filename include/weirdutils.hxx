a9fc58db26156fa65101f9362f656573e1486b86
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-09-10 10:49:12 -0400

    Add files via upload

---
+++ include/weirdutils.hxx
@@ -0,0 +1,10 @@
+#ifndef IDT_WEIRD_UTILS_HXX
+#define IDT_WEIRD_UTILS_HXX
+
+#include <weirdutils/exception.hxx>
+#include <weirdutils/data.hxx>
+#include <weirdutils/dlfunctions.h>
+#include <weirdutils/dynamicload.hxx>
+#include <weirdutils/string.hxx>
+
+#endif
