94af819038a1fad83117b85c62b23f032df21fdc
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-09-11 10:31:53 +0000

    Update string.hxx

--- include/weirdutils/string.hxx
+++ include/weirdutils/string.hxx
@@ -14,26 +14,26 @@ extern "C" {
 namespace weird {
     typedef std::vector<std::string> stringvec_t;
     
-    IDT_WEIRD_API bool        is_prefix(std::string& target_, std::string prefix_);
-    IDT_WEIRD_API bool        is_suffix(std::string& target_, std::string suffix_);
-    IDT_WEIRD_API std::string substring(std::string& target_, std::size_t fromLeft_, std::size_t toRight);
-    IDT_WEIRD_API stringvec_t split(std::string& target_, std::string& pattern_);
+    IDT_WEIRD_API bool        is_prefix(std::string target_, std::string prefix_);
+    IDT_WEIRD_API bool        is_suffix(std::string target_, std::string suffix_);
+    IDT_WEIRD_API std::string substring(std::string target_, std::size_t fromLeft_, std::size_t toRight);
+    IDT_WEIRD_API stringvec_t split(std::string target_, std::string pattern_);
+    
+    IDT_WEIRD_API bool is_double(std::string target_);
+    IDT_WEIRD_API bool is_integer(std::string target_);
+    IDT_WEIRD_API bool is_boolean(std::string target_);
+    IDT_WEIRD_API bool is_x8(std::string target_);
+    IDT_WEIRD_API bool is_x16(std::string target_);
+    IDT_WEIRD_API bool is_x32(std::string target_);
+    IDT_WEIRD_API bool is_x64(std::string target_);
 
-    IDT_WEIRD_API bool is_float(std::string& target_);
-
-    IDT_WEIRD_API bool is_integer(std::string& target_);
-    IDT_WEIRD_API bool is_x8(std::string& target_);
-    IDT_WEIRD_API bool is_x16(std::string& target_);
-    IDT_WEIRD_API bool is_x32(std::string& target_);
-    IDT_WEIRD_API bool is_x64(std::string& target_);
-
-    IDT_WEIRD_API float        to_float(std::string& target_);
-    IDT_WEIRD_API int          to_integer(std::string& target_);
-
-    IDT_WEIRD_API std::uint8_t  to_u8(std::string& target_);
-    IDT_WEIRD_API std::uint16_t to_u16(std::string& target_);
-    IDT_WEIRD_API std::uint32_t to_u32(std::string& target_);
-    IDT_WEIRD_API std::uint64_t to_u64(std::string& target_);
+    IDT_WEIRD_API int           to_integer(std::string target_);
+    IDT_WEIRD_API double        to_double(std::string target_);
+    IDT_WEIRD_API int           to_boolean(std::string target_);
+    IDT_WEIRD_API std::uint8_t  to_u8(std::string target_);
+    IDT_WEIRD_API std::uint16_t to_u16(std::string target_);
+    IDT_WEIRD_API std::uint32_t to_u32(std::string target_);
+    IDT_WEIRD_API std::uint64_t to_u64(std::string target_);
 }
 
 #endif
